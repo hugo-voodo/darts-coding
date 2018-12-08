a0e2904aa6dc9738925e225a0ae159e27963dce3
Thomy Phan <phant@cip.ifi.lmu.de>
2018-12-08 13:00:20 +0100

    Add preliminary code

---
+++ session1/agent.py
@@ -0,0 +1,11 @@
+class Agent:
+
+    def __init__(self):
+        pass
+        
+    def policy(self, state):
+        pass
+        
+    def update(self, state, action, next_state, reward):
+        pass
+        
